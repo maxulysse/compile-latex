1ee2ae30dc290f8c052b3322a02a71487fab19b8
MaxUlysse <max@ithake.eu>
2017-02-16 13:47:58 +0100

    add script to build singularity image

---
+++ singularity.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v /tmp/compile-beamer:/output \
+--privileged -t --rm \
+singularityware/docker2singularity \
+maxulysse/compile-beamer:1.3
