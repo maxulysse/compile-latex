ce071200a799ba24a04343b043acd344004797d5
MaxUlysse <max@ithake.eu>
2017-05-03 11:21:35 +0200

    change ./nextflow to nextflow

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting Nextflow... Command:"
-echo "./nextflow run . -profile docker"
+echo "nextflow run . -profile docker"
 echo "-----"
-./nextflow run . -profile docker
+nextflow run . -profile docker
