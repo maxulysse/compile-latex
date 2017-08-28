d8d07dc48908f0fb6712ef11133ec1d642384291
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-28 14:23:07 +0200

    better bash script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+set -xeuo pipefail
+
+PROFILE="docker"
+
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+    case $key in
+        -p|--profile)
+        PROFILE="$2"
+        shift
+        ;;
+        *) # unknown option
+        ;;
+    esac
+    shift
+done
+
+
 nextflow run . -profile $1
