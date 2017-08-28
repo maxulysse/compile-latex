8ecfd6ce6d16e92915a73a30428c215bfa256a45
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-28 15:17:14 +0200

    indentation

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,20 +4,20 @@ PROFILE="docker"
 
 while [[ $# -gt 1 ]]
 do
-    key="$1"
-    case $key in
-        -i|--install)
-        INSTALL=true
-        shift
-        -p|--profile)
-        PROFILE="$2"
-        shift
-        ;;
-        ;;
-        *) # unknown option
-        ;;
-    esac
+  key="$1"
+  case $key in
+    -i|--install)
+    INSTALL=true
     shift
+    ;;
+    -p|--profile)
+    PROFILE="$2"
+    shift
+    ;;
+    *) # unknown option
+    ;;
+  esac
+  shift
 done
 
 # Install Singularity
