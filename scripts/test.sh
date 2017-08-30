1de7c7983024135989d265445c6975fd332aa698
Maxime Garcia <max.u.garcia@gmail.com>
2017-08-30 12:15:33 +0200

    fix install

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+INSTALL=false
 PROFILE="docker"
 
 while [[ $# -gt 0 ]]
 do
   key="$1"
   case $key in
+    -i|--install)
+    INSTALL=true
+    ;;
     -p|--profile)
     PROFILE="$2"
     shift
@@ -14,6 +18,12 @@ do
   esac
   shift
 done
+
+# Install Singularity
+if [[ "$PROFILE" == singularity ]] && [[ "$INSTALL" == true ]]
+then
+  ./scripts/install.sh -t singularity
+fi
 
 # Run test
 nextflow run . -profile $PROFILE
