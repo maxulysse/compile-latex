769ac153e6bec1172a291fb8ec4071228be2882f
Maxime Garcia <max.u.garcia@gmail.com>
2017-10-12 16:28:04 +0200

    add --tag and --tex option

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 set -xeuo pipefail
 
-PROFILE="docker"
+PROFILE=docker
+TAG=1.7.1
+TEX=sample.tex
 
 while [[ $# -gt 0 ]]
 do
-  key="$1"
+  key=$1
   case $key in
     -p|--profile)
-    PROFILE="$2"
-    shift
+    PROFILE=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --tag)
+    TAG=$2
+    shift # past argument
+    shift # past value
+    ;;
+    --tex)
+    TEX=$2
+    shift # past argument
+    shift # past value
     ;;
     *) # unknown option
+    shift # past argument
     ;;
   esac
-  shift
 done
 
 # Run test
-nextflow run . -profile $PROFILE
+nextflow run . -profile ${PROFILE} --tag ${tag} --tex ${TEX}
